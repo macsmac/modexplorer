00c31809722b3e6533ac572376b8e1ae266abdb4
macsmac <bywildeme@gmail.com>
2017-11-23 02:31:02 +0500

    Добавил 'модпаки'

--- src/cli.js
+++ src/cli.js
@@ -3,6 +3,7 @@ const curse = require("./curseforge");
 const clear = require("clear");
 const size = require("window-size");
 const fs = require("fs");
+const https = require("https");
 const zip = require("./zip");
 const signature = require("./signature");
 const async = require("async");
@@ -34,7 +35,7 @@ function mainScreen() {
 		{
 			message: "Чего хотел?",
 			name: "btn",
-			choices: ["Поиск модов", "Проверить цифровую подпись (Experimental feature)"],
+			choices: ["Поиск модов", "Проверить цифровую подпись (Experimental feature)", "Экспортировать/импортировать модпаки"],
 			suffix: "",
 			prefix: "",
 			type: "list"
@@ -42,16 +43,17 @@ function mainScreen() {
 	]).then(function(data) {
 		if (data.btn === "Поиск модов") versionScreen();
 		else if (data.btn === "Проверить цифровую подпись (Experimental feature)") signatureScreen();
+		else if (data.btn === "Экспортировать/импортировать модпаки") modpackScreen();
 	});
 }
 
-function versionScreen() {
+function versionScreen(cb, endCb) {
 	clear();
 	inquirer.prompt([
 		{
 			name: "version",
 			message: "Выберите версию: ",
-			choices: [...versions, "[Назад в меню]"],
+			choices: [...versions, "[Назад]"],
 			suffix: "",
 			prefix: "",
 			type: "list",
@@ -61,32 +63,36 @@ function versionScreen() {
 		if (data.version === "[Назад в меню]") return process.nextTick(() => mainScreen());
 
 		version = curse.versions[data.version];
-		categoriesScreen();
+		categoriesScreen(cb, endCb);
 	});
 }
 
-function categoriesScreen() {
+function categoriesScreen(cb, endCb) {
 	clear();
 	inquirer.prompt([
 		{
 			name: "cat",
 			message: "Категория:",
-			choices: ["[Строка поиска]", ...categories, "[Назад в меню]"],
+			choices: ["[Строка поиска]", ...categories, !endCb ? "[Назад в меню]" : "[Экспортировать]"],
 			suffix: "",
 			prefix: "",
 			type: "list",
 			pageSize: size.height - 4
 		}
 	]).then(function(data) {
-		if (data.cat === "[Назад в меню]") return process.nextTick(() => mainScreen());
-		else if (data.cat === "[Строка поиска]") return process.nextTick(() => searchScreen());
+		// полюбому надо рефакторить 
+		// Но потом ;3
+		if (data.cat === "[Экспортировать]") return process.nextTick(() => endCb());
+		else if (data.cat === "[Назад в меню]") return process.nextTick(() => mainScreen());
+		else if (data.cat === "[Строка поиска]") 
+			return process.nextTick(() => searchScreen(cb, endCb));
  
 		category = _categories[data.cat];
-		modBrowse(1, category, version);
+		modBrowse(1, category, version, undefined, cb, endCb);
 	});
 }
 
-function searchScreen(text) {
+function searchScreen(cb, endCb) {
 	clear();
 	console.log(magic.blue, "\n");
 
@@ -100,9 +106,9 @@ function searchScreen(text) {
 			prefix: ""
 		}
 	]).then(function(data) {
-		if (data.search === "") return process.nextTick(() => mainScreen());
+		if (data.search === "") return process.nextTick(() => categoriesScreen(cb, endCb));
 
-		modBrowse(1, null, version, data.search);
+		modBrowse(1, null, version, data.search, cb, endCb);
 	});
 }
 
@@ -181,7 +187,7 @@ function startSignCheck() {
 	});
 }
 
-function modBrowse(page, category, version, search) {
+function modBrowse(page, category, version, search, cbFileBrowse, endCb) {
 	clear();
 	console.log("Загружаю...");
 
@@ -203,13 +209,14 @@ function modBrowse(page, category, version, search) {
 				pageSize: size.height
 			}
 		]).then(function(data) {
-			if (data.mod === "[Назад в категории]") process.nextTick(() => categoriesScreen());
-			else if (data.mod === "[<= Пред. стр]") process.nextTick(() => modBrowse(page - 1, category, version, search));
-			else if (data.mod === "[След. стр =>]") process.nextTick(() => modBrowse(page + 1, category, version, search));
-			else if (data.mod === "[Назад к поиску]") process.nextTick(() => searchScreen(search));
-			else fileBrowse(mods.find(e => e.title === data.mod.split("|")[0]), function() {
-				process.nextTick(() => modBrowse(page, category, version, search));
-			});
+			if (data.mod === "[Назад в категории]") process.nextTick(() => categoriesScreen(cbFileBrowse, endCb));
+			else if (data.mod === "[<= Пред. стр]") process.nextTick(() => modBrowse(page - 1, category, version, search, cbFileBrowse, endCb));
+			else if (data.mod === "[След. стр =>]") process.nextTick(() => modBrowse(page + 1, category, version, search, cbFileBrowse, endCb));
+			else if (data.mod === "[Назад к поиску]") process.nextTick(() => searchScreen(cbFileBrowse, endCb));
+			else fileBrowse(mods.find(e => e.title === data.mod.split("|")[0]), !cbFileBrowse, function(link) {
+				if (cbFileBrowse) cbFileBrowse(link);
+				process.nextTick(() => modBrowse(page, category, version, search, cbFileBrowse, endCb));
+			})
 		});
 	}
 
@@ -219,7 +226,7 @@ function modBrowse(page, category, version, search) {
 	});
 }
 
-function fileBrowse(mod, cb) {
+function fileBrowse(mod, dl, cb) {
 	clear();
 	console.log("Загружаю...");
 	curse.getAllFiles(mod.rawLink, version, function(files) {
@@ -240,11 +247,91 @@ function fileBrowse(mod, cb) {
 
 			const filename = "../mods/" + data.file + (data.file.endsWith(".jar") ? "" : ".jar");
 
-			clear();
-			console.log("Скачиваю: " + data.file + "\nСюда: " + filename);
+			const mod = files.find(e => e.title === data.file).rawLink;
 
-			curse.downloadMod(files.find(e => e.title === data.file).rawLink, filename, cb);
+			if (dl) {
+				clear();
+				console.log("Скачиваю: " + data.file + "\nСюда: " + filename);
+
+				curse.downloadMod(mod, filename, cb);
+			} else {
+				clear();
+				console.log("Получаю ссылку...");
+				curse.fetchCDNLink(mod, cb);
+			}
 		});
+	});
+}
+
+function modpackScreen() {
+	clear();
+
+	console.log("Тут вы можете экспортировать и импортировать \"модпаки\"");
+	console.log("Конкретно в ЭТОЙ версии это просто список модов .mpackf файле которые требуется скачать");
+	console.log("Как юзать:".green);
+	console.log("  1. Жмете 'Экспортировать'");
+	console.log("  2. Выбираете нужные вам моды");
+	console.log("  3. Когда закончите в категориях жмете [Экспортировать]");
+	console.log("  4. В папке modpacks появится файл <случайные символы>.mpackf");
+	console.log("  5. Переименовываете его как хотите и кидаете куда хотите");
+	console.log("Как импортировать:".green);
+	console.log("  1. Кидаете модпак в папку modpacks");
+	console.log("  2. Жмете Импортировать и выбираете ваш файл");
+	console.log("  3. Ждете пока скачаются все моды. Они будут в папке mods");
+
+	inquirer.prompt([
+		{
+			type: "list",
+			name: "btn",
+			choices: ["Экспортировать", "Импортировать", "Назад"],
+			suffix: "",
+			prefix: "",
+			message: " "
+		}
+	]).then(function(data) {
+		if (data.btn === "Экспортировать") {
+			let links = [];
+
+			versionScreen(e => links.push(e), function() {
+				clear();
+				console.log("Экспортирую...");
+				fs.writeFileSync("../modpacks/" + Math.random().toString(36).slice(2) + ".mpackf", links.join("\n"));
+				console.log("Готово ;3");
+			});
+		} else if (data.btn === "Импортировать") {
+			clear();
+
+			const files = fs.readdirSync("../modpacks");
+
+			inquirer.prompt([{
+				type: "list",
+				name: "file",
+				choices: files,
+				suffix: "",
+				prefix: "",
+				message: "Файл модпака:"
+			}, {
+				type: "confirm",
+				name: "check",
+				message: "Проверить подписи модов?"
+			}]).then(function(data, cb) {
+				const modpack = fs.readFileSync("../modpacks/" + data.file);
+				const files = modpack.toString().split("\n").filter(e => e);
+
+				clear();
+
+				async.mapSeries(files, function(link, cb) {
+					const filename = link.split("/").pop();
+
+					console.log("Качаю:", filename);
+
+					https.get(link, s => s.pipe(fs.createWriteStream("../mods/" + filename)).on("close", cb));
+				}, function() {
+					if (data.check) startSignCheck();
+				});
+			});
+		}
+		else process.nextTick(() => mainScreen());
 	});
 }
 

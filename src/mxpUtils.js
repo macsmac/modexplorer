9835953decc2188a08fc533f84ecba6625246a74
macsmac <bywildeme@gmail.com>
2017-11-26 18:09:58 +0500

    Добавил кэширование

--- src/mxpUtils.js
+++ src/mxpUtils.js
@@ -1,15 +1,22 @@
 const fs = require("fs"),
+	  fse = require("fs-extra"),
 	  https = require("https"),
 	  crypto = require("crypto");
 
 const mxpUtils = {
 	downloadFile: function(link, dest, cb) {
-		https.get(link, 
-			stream => 
-				stream.pipe(fs.createWriteStream(dest))
-					.on("close", () => cb = cb())
-					.on("error", cb)
-		);
+		const cachefile = "../cache/" + mxpUtils.md5(link);
+
+		fs.stat(cachefile, function(err, stats) {
+			if (stats && stats.isFile()) return fse.copy(cachefile, dest, cb);
+
+			https.get(link, 
+				stream => 
+					stream.pipe(fs.createWriteStream(dest))
+						.on("close", () => fse.copy(dest, cachefile, cb))
+						.on("error", cb)
+			);
+		});
 	},
 	randStr: function() {
 		return Math.random().toString(36).slice(2);
